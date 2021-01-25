76825235d1fe758090e758097cecdbe72075e698
DastanatsaD <77788357+DastanatsaD@users.noreply.github.com>
2021-01-25 13:11:08 +0600

    Dastan

---
+++ main.py
@@ -0,0 +1,24 @@
+a = int(input('Число I:'))
+x = input("Знак")
+b = int(input('Число II:'))
+if x == '-':
+    print('Ответ:', a-b)
+elif x == '+':
+    print('Ответ:', a+b)
+elif x == '/':
+    print('Ответ:', a/b)
+elif x == '*':
+    print('Ответ:', a*b)
+elif x == '%':
+    print('Ответ:', a%b)
+elif x == '//':
+    print('Ответ:', a//b)
+elif x == '**':
+    print('Ответ:', a**b)
+elif x == '>':
+    print('Ответ:', a>b)
+elif x == '!=':
+    print('Ответ:', a!=b)
+
+else:
+    print("Повторите еще раз")
