977b3ac7a4aa2598e3a95907c06ce6a08c376875
DastanatsaD <77788357+DastanatsaD@users.noreply.github.com>
2021-01-28 15:37:43 +0600

    Moditication

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-a = int(input('Число I:'))
-x = input("Знак")
-b = int(input('Число II:'))
-if x == '-':
-    print('Ответ:', a-b)
-elif x == '+':
-    print('Ответ:', a+b)
-elif x == '/':
-    print('Ответ:', a/b)
-elif x == '*':
-    print('Ответ:', a*b)
-elif x == '%':
-    print('Ответ:', a%b)
-elif x == '//':
-    print('Ответ:', a//b)
-elif x == '**':
-    print('Ответ:', a**b)
-
-else:
-    print("Повторите еще раз")
+isWorking = True
+while(isWorking==True):
+    a = int(input('Число I:'))
+    x = input("Знак")
+    b = int(input('Число II:'))
+    if x == '-':
+        print('Ответ:', a-b)
+    elif x == '+':
+        print('Ответ:', a+b)
+    elif x == '/':
+        print('Ответ:', a/b)
+    elif x == '*':
+        print('Ответ:', a*b)
+    elif x == '%':
+        print('Ответ:', a%b)
+    elif x == '//':
+        print('Ответ:', a//b)
+    elif x == '**':
+        print('Ответ:', a**b)
+    else:
+        print("'Неправильный знак.' \n 'Повторите еще раз.'")
